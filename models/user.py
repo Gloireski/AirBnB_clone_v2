7db52035e4eae3892f43836d077de2f3036470a8
Gloireski <belemgloirebekoutou8@gmail.com>
2024-01-18 11:44:38 -0800

    task 6 update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """
+    This class defines a user by various attributes
+
+    Attributes:
+        __tablename__ (str): the name of the Mysql table to store users.
+        email (sqlalchemy string): user's mail adress
+        password (sqlalchemy string): user's password
+        first_name (sqlalchemy string): user's first name
+        last_name (sqlalcheny string): user's last name
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
