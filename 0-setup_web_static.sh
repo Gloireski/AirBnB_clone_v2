1f657a490b50e23d6957c89c42cd1188b420d2e3
Gloireski <belemgloirebekoutou8@gmail.com>
2024-02-01 08:15:02 -0800

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,13 @@ apt-get install nginx -y
 
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
