05953a62bee26ab5d87c171f3502f67f60846c12
Gloireski <belemgloirebekoutou8@gmail.com>
2024-02-16 22:09:03 +0000

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: displays 'Hello HBNB!'
+    /hbnb: displays 'HBNB'
+    /c/<text>: display C followed by the value of the text variable
+               (replace underscore _ symbols with a space )
+    /python/<text>: display Python, followed by the value of the text variable
+               (replace underscore _ symbols with a space ) 
+"""
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
 	return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-	return "HBNB"
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-	return "C {}".format(text.replace("_", " "))
+    return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
-        return "Python {}".format(text.replace("_", " "))
+    return "Python {}".format(text.replace("_", " "))
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
