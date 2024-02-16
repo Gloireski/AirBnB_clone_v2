c4dbc1ba66953d7ef7537525433953f76b98b223
Gloireski <belemgloirebekoutou8@gmail.com>
2024-02-16 22:14:03 +0000

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,8 +7,8 @@ Routes:
     /hbnb: displays 'HBNB'
     /c/<text>: display C followed by the value of the text variable
                (replace underscore _ symbols with a space )
-    /python/<text>: display Python, followed by the value of the text variable
-               (replace underscore _ symbols with a space ) 
+    /python/<text>: display Python, followed by the value of the text
+               variable (replace underscore _ symbols with a space ) 
 """
 from flask import Flask
 
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-	return "Hello HBNB!"
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
