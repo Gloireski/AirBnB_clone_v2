84817fa6d2cbc24a96e1f6f70dc22e91d7972178
Gloireski <belemgloirebekoutou8@gmail.com>
2024-02-19 02:35:43 +0000

    fix 8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,17 +3,6 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: displays 'Hello HBNB!'
-    /hbnb: displays 'HBNB'
-    /c/<text>: display C followed by the value of the text variable
-               (replace underscore _ symbols with a space )
-    /python/<text>: display Python, followed by the value of the text
-               variable (replace underscore _ symbols with a space )
-    /number/<n>: display n is a number only if n is an integer
-    /number_template/<n>: display a HTML page only if n is an integer:
-    H1 tag: Number: n inside the tag BODY
-    /number_odd_or_even/<n>: display a HTML page only if n is an integer:
-    H1 tag: Number: n is even|odd inside the tag BODY
     /states_list: display a HTML page: (inside the tag BODY)
         H1 tag: States
         UL tag: with the list of all State objects present in DBStorage
@@ -30,12 +19,17 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
     all_states = storage.all(State)
     return render_template('7-states_list.html', states=all_states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
